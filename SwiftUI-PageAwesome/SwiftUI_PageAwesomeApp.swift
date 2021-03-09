e41ddf09ce62a4ce190baae9f9a9f5b310b097a6
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 16:33:34 +0700

    Initial Commit

---
+++ SwiftUI-PageAwesome/SwiftUI_PageAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PageAwesomeApp.swift
+//  SwiftUI-PageAwesome
+//
+//  Created by BillY-MAC on 9/3/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PageAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
