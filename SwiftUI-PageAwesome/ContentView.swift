f3c8315aaa71e4bda8565e1f88efcea03401836d
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 17:02:04 +0700

    Add SwiftUI page awesome

--- SwiftUI-PageAwesome/ContentView.swift
+++ SwiftUI-PageAwesome/ContentView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .center) {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(spacing: 5) {
+                Spacer()
+                
+                Text("วนอุทยานภูลังกา จังหวัดพะเยา")
+                    .font(.system(size: 32,weight: .bold,design: .default))
+                    .foregroundColor(Color.white)
+                    .padding(.bottom,25)
+                    .shadow(color: Color.black,radius: 2,x: 2,y: 2)
+                
+                
+                Text("เว็บไซต์ : กรมอุทยานแห่งชาติ สัตว์ป่า และพันธุ์พืช,\nส่วนตำบลผาช้างน้อย และ เฟซบุ๊ก องค์การบริหารส่วน\nตำบลผาช้างน้อย")
+                    .font(.system(size: 18,weight: .medium))
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .shadow(color: .gray, radius: 2,x:2,y:2)
+                
+                Spacer()
+                
+                Button {
+                    print("tapp!")
+                } label: {
+                    Text("Awesome Button")
+                        .cornerRadius(50)
+                        .frame(width: 320, height: 60)
+                        .font(.system(size: 24,weight: .bold,design: .default))
+                        .foregroundColor(Color.white)
+                        
+                }.background(LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                .cornerRadius(20)
+                .padding(.bottom,40)
+            }
+        }
     }
 }
 
